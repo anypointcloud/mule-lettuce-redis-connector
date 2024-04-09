91f79e9d44c89ae8f0a31ca75dd84511af133620
Ryan Hoegg <ryan.hoegg@gmail.com>
2024-04-09 12:27:35 -0500

    support java 17 and make the ZADD UI a little nicer

--- src/main/java/cloud/anypoint/redis/internal/LettuceRedisExtension.java
+++ src/main/java/cloud/anypoint/redis/internal/LettuceRedisExtension.java
@@ -1,12 +1,17 @@
 package cloud.anypoint.redis.internal;
+import static org.mule.sdk.api.meta.JavaVersion.JAVA_11;
+import static org.mule.sdk.api.meta.JavaVersion.JAVA_17;
+import static org.mule.sdk.api.meta.JavaVersion.JAVA_8;
 
 import org.mule.runtime.extension.api.annotation.Configurations;
 import org.mule.runtime.extension.api.annotation.Extension;
 import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
+import org.mule.sdk.api.annotation.JavaVersionSupport;
 
 @Xml(prefix = "lettuce")
-@Extension(name = "Lettuce Redis Configuration", vendor = "Anypoint Cloud")
+@Extension(name = "Lettuce Redis", vendor = "Anypoint Cloud")
 @Configurations(RedisConfiguration.class)
+@JavaVersionSupport({JAVA_8, JAVA_11, JAVA_17})
 public class LettuceRedisExtension {
 
 }
--- src/main/java/cloud/anypoint/redis/internal/operation/RedisOperations.java
+++ src/main/java/cloud/anypoint/redis/internal/operation/RedisOperations.java
@@ -1,44 +1,55 @@
 package cloud.anypoint.redis.internal.operation;
 
 import cloud.anypoint.redis.internal.connection.LettuceRedisConnection;
-import cloud.anypoint.redis.api.operation.args.ZAddArguments;
 import io.lettuce.core.ScoredValue;
 import io.lettuce.core.ZAddArgs;
+import org.mule.runtime.extension.api.annotation.Alias;
 import org.mule.runtime.extension.api.annotation.param.Connection;
 import org.mule.runtime.extension.api.annotation.param.Content;
+import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
 import org.mule.runtime.extension.api.runtime.operation.Result;
 import org.mule.runtime.extension.api.runtime.process.CompletionCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
 
 import java.util.Map;
 
 
 public class RedisOperations {
+
+  private final Logger LOGGER = LoggerFactory.getLogger(RedisOperations.class);
   public void jsonSet(@Connection LettuceRedisConnection connection) {
     // dispatch
   }
 
+  @DisplayName("ZADD")
   public void zadd(@Connection LettuceRedisConnection connection,
                    String key,
-                   ZAddArguments options,
+                   @DisplayName("XX") boolean xx,
+                   @DisplayName("NX") boolean nx,
+                   @DisplayName("GT") boolean gt,
+                   @DisplayName("LT") boolean lt,
+                   @DisplayName("CH") boolean ch,
                    @Content Map<String, Double> memberScores,
                    CompletionCallback<Long, Void> callback) {
     ScoredValue<String>[] scoredValues = memberScores.entrySet().stream()
             .map((entry) -> ScoredValue.just(entry.getValue(), entry.getKey()))
             .toArray(ScoredValue[]::new);
     ZAddArgs args = new ZAddArgs();
-    if (options.isXx()) {
+    if (xx) {
       args = args.xx();
     }
-    if (options.isNx()) {
+    if (nx) {
       args = args.nx();
     }
-    if (options.isGt()) {
+    if (gt) {
       args = args.gt();
     }
-    if (options.isLt()) {
+    if (lt) {
       args = args.lt();
     }
-    if (options.isCh()) {
+    if (ch) {
       args = args.ch();
     }
     connection.commands().zadd(key, args, scoredValues)
