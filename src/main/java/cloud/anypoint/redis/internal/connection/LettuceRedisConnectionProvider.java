288259124fc7aeca1d2eb44f719bdbe9afbbdac9
Ryan Hoegg <ryan.hoegg@gmail.com>
2024-04-11 17:43:07 -0500

    add connectivity test

--- src/main/java/cloud/anypoint/redis/internal/connection/LettuceRedisConnectionProvider.java
+++ src/main/java/cloud/anypoint/redis/internal/connection/LettuceRedisConnectionProvider.java
@@ -13,6 +13,7 @@ import org.mule.runtime.extension.api.annotation.param.Parameter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
 
 public class LettuceRedisConnectionProvider implements CachedConnectionProvider<LettuceRedisConnection>, Initialisable, Disposable {
 
@@ -34,12 +35,15 @@ public class LettuceRedisConnectionProvider implements CachedConnectionProvider<
   @Override
   public void initialise() throws InitialisationException {
     try {
+      // workaround for similar issue to this: https://github.com/redis/lettuce/issues/1767
+      System.setProperty("io.lettuce.core.jfr", "false");
       RedisURI uri = RedisURI.Builder
               .redis(host, port)
               .withSsl(tls)
               .withPassword(password) // TODO: CredentialsProvider to allow expressions
               .build();
       this.redisClient = RedisClient.create(uri);
+
     }
     catch (IllegalStateException e) {
       throw new InitialisationException(createStaticMessage(e.getLocalizedMessage()), e, this);
@@ -63,7 +67,13 @@ public class LettuceRedisConnectionProvider implements CachedConnectionProvider<
 
   @Override
   public ConnectionValidationResult validate(LettuceRedisConnection connection) {
-    return ConnectionValidationResult.success();
+    return connection.commands().ping()
+            .map((r) -> ConnectionValidationResult.success())
+            .onErrorResume( (t) ->
+                    Mono.just(ConnectionValidationResult.failure(
+                            t.getMessage(),
+                            new Exception(t))))
+            .block();
   }
 
 }
