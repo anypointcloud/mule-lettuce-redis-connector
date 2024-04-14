13576c670f891ac64d4fc82ae31ef769ee85e741
Ryan Hoegg <ryan.hoegg@gmail.com>
2024-04-13 23:38:09 -0500

    add SET operation

--- src/main/java/cloud/anypoint/redis/internal/operation/RedisOperations.java
+++ src/main/java/cloud/anypoint/redis/internal/operation/RedisOperations.java
@@ -1,18 +1,14 @@
 package cloud.anypoint.redis.internal.operation;
 
 import cloud.anypoint.redis.internal.commands.CommandReturnType;
-import cloud.anypoint.redis.internal.commands.RuntimeCommand;
 import cloud.anypoint.redis.internal.connection.LettuceRedisConnection;
 import cloud.anypoint.redis.internal.metadata.DynamicCommandOutputTypeResolver;
 import io.lettuce.core.ScoredValue;
+import io.lettuce.core.SetArgs;
 import io.lettuce.core.ZAddArgs;
-import io.lettuce.core.codec.StringCodec;
-import io.lettuce.core.output.StatusOutput;
-import io.lettuce.core.protocol.Command;
 import org.mule.runtime.extension.api.annotation.metadata.MetadataKeyId;
 import org.mule.runtime.extension.api.annotation.metadata.OutputResolver;
-import org.mule.runtime.extension.api.annotation.param.Connection;
-import org.mule.runtime.extension.api.annotation.param.Content;
+import org.mule.runtime.extension.api.annotation.param.*;
 import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
 import org.mule.runtime.extension.api.runtime.operation.Result;
 import org.mule.runtime.extension.api.runtime.process.CompletionCallback;
@@ -24,7 +20,6 @@ import java.util.Map;
 
 
 public class RedisOperations {
-
     private final Logger LOGGER = LoggerFactory.getLogger(RedisOperations.class);
 
     @OutputResolver(output = DynamicCommandOutputTypeResolver.class)
@@ -33,6 +28,7 @@ public class RedisOperations {
                             List<String> arguments,
                             @MetadataKeyId CommandReturnType returnType,
                             CompletionCallback<Object, Void> callback) {
+
         connection.customCommands().dynamic(command, arguments, returnType)
                 .subscribe(
                         (result) -> callback.success(
@@ -43,15 +39,38 @@ public class RedisOperations {
                 );
     }
 
+    @DisplayName("SET")
+    @MediaType(value = MediaType.TEXT_PLAIN, strict = false)
+    public void set(@Connection LettuceRedisConnection connection,
+                    String key,
+                    @Content String value,
+                    @Optional @DisplayName("XX") boolean xx,
+                    @Optional @DisplayName("NX") boolean nx,
+                    @Optional @DisplayName("GET") boolean get,
+                    @Optional @DisplayName("EX") Integer ex,
+                    @Optional @DisplayName("PX") Integer px,
+                    @Optional @DisplayName("EXAT") Integer exat,
+                    @Optional @DisplayName("PXAT") Integer pxat,
+                    @Optional @DisplayName("KEEPTTL") boolean keepttl,
+                    CompletionCallback<String, Void> callback) {
+        SetArgs args = new SetArgs();
+        connection.commands().set(key, value, args)
+                .subscribe(
+                        result -> callback.success(Result.<String, Void>builder()
+                                .output(result)
+                                .build()),
+                        callback::error);
+    }
+
     @DisplayName("ZADD")
     public void zadd(@Connection LettuceRedisConnection connection,
                      String key,
+                     @Content Map<String, Double> memberScores,
                      @DisplayName("XX") boolean xx,
                      @DisplayName("NX") boolean nx,
                      @DisplayName("GT") boolean gt,
                      @DisplayName("LT") boolean lt,
                      @DisplayName("CH") boolean ch,
-                     @Content Map<String, Double> memberScores,
                      CompletionCallback<Long, Void> callback) {
         ScoredValue<String>[] scoredValues = memberScores.entrySet().stream()
                 .map((entry) -> ScoredValue.just(entry.getValue(), entry.getKey()))
@@ -74,7 +93,7 @@ public class RedisOperations {
         }
         connection.commands().zadd(key, args, scoredValues)
                 .subscribe(
-                        (result) -> callback.success(
+                        result -> callback.success(
                                 Result.<Long, Void>builder()
                                         .output(result)
                                         .build()),
