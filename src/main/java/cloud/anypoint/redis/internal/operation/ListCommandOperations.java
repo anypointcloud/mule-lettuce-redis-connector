05bf69f9ad80a6dadf0ee3fc5f7af2a355cc7bf6
Ryan Hoegg <ryan.hoegg@gmail.com>
2024-04-30 13:29:44 -0500

    add LPOP

--- src/main/java/cloud/anypoint/redis/internal/operation/ListCommandOperations.java
+++ src/main/java/cloud/anypoint/redis/internal/operation/ListCommandOperations.java
@@ -5,19 +5,23 @@ import cloud.anypoint.redis.internal.connection.LettuceRedisConnection;
 import cloud.anypoint.redis.internal.exception.ArgumentException;
 import cloud.anypoint.redis.internal.metadata.ArgumentErrorTypeProvider;
 import cloud.anypoint.redis.internal.metadata.WrongTypeErrorTypeProvider;
+import com.google.common.base.Functions;
 import org.mule.runtime.extension.api.annotation.error.Throws;
-import org.mule.runtime.extension.api.annotation.param.Connection;
-import org.mule.runtime.extension.api.annotation.param.Content;
+import org.mule.runtime.extension.api.annotation.metadata.MetadataKeyId;
+import org.mule.runtime.extension.api.annotation.metadata.OutputResolver;
+import org.mule.runtime.extension.api.annotation.param.*;
 import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
 import org.mule.runtime.extension.api.runtime.operation.Result;
 import org.mule.runtime.extension.api.runtime.process.CompletionCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ListCommandOperations {
-    private final Logger LOGGER = LoggerFactory.getLogger(SortedSetCommandOperations.class);
+    private final Logger LOGGER = LoggerFactory.getLogger(ListCommandOperations.class);
 
     @DisplayName("LPUSH")
     @Throws({WrongTypeErrorTypeProvider.class, ArgumentErrorTypeProvider.class})
@@ -36,5 +40,23 @@ public class ListCommandOperations {
         catch (IllegalArgumentException e) {
             callback.error(new ArgumentException("LPUSH", e));
         }
+    }
+
+    @DisplayName("LPOP")
+    @Throws(WrongTypeErrorTypeProvider.class)
+    public void lpop(@Connection LettuceRedisConnection connection,
+                     String key,
+                     @MetadataKeyId @Optional Integer count,
+                     CompletionCallback<List<String>, Void> callback) {
+        Mono<List<String>> cmd = connection.commands().lpop(key).map(val -> Arrays.asList(val));
+        if (null != count) {
+//            if (count < 0)
+            cmd = connection.commands().lpop(key, count).collectList().map(Functions.identity());
+        }
+        mapWrongTypeError(cmd, "LPOP", key).subscribe(
+            result -> callback.success(Result.<List<String>, Void>builder()
+                    .output(result)
+                    .build()),
+            callback::error);
     }
 }
